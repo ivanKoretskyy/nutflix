66003d67b2e17a366d7cc621199541c58ae74d6d
Ivan Koretskyy <ivankoretskyy@gmail.com>
2023-10-10 18:21:42 -0500

    Added initial files

---
+++ src/test/java/com/example/nutflix/NutflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
